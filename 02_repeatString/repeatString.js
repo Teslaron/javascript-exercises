04af4c68568036e0a9f5bc6b41616af84ffa2621
Teslaron <117548948+Teslaron@users.noreply.github.com>
2022-11-18 19:13:50 +0100

    Finished exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(a, b) {
+    let c = "";
+    for (let i=0; i < b; i++) {
+        c += a;
+    }
+    if (b < 0) {
+    return 'ERROR'
+    }
+    else {
+    return c;    
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
