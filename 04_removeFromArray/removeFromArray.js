8a19a2168e280abd7bfa86e44328b8a023dff64a
Teslaron <117548948+Teslaron@users.noreply.github.com>
2022-11-18 20:55:42 +0100

    Finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayInput, a, b, c, d) {
+    let array = arrayInput;
+    let forDeletion =[a, b, c, d];
+    return array.filter(item => !forDeletion.includes(item));
 };
 
 // Do not edit below this line
