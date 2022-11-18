c1208998a1c64bc4120147cfe51d5b8a5a36a0ed
Teslaron <117548948+Teslaron@users.noreply.github.com>
2022-11-18 21:21:27 +0100

    Finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numb1, numb2) {
+    let finalSum = 0;
+    if ((numb1>0&&numb2>0) && (typeof numb1 == 'number' && typeof numb2 == 'number')) {
+        if (numb1 < numb2) {
+            for (i=0; i<=numb2; i++) {
+                finalSum +=i;
+            }
+        }
+        else if(numb1 > numb2) {
+            for (i=0; i<=numb1; i++) {
+                finalSum +=i;
+            }
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
