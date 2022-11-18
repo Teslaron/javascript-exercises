928402993f90134bad6e2d83ed1e720e43c265ab
Teslaron <117548948+Teslaron@users.noreply.github.com>
2022-11-18 19:34:36 +0100

    Finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let stringlength = string.length;
+let reverse = "";
+    for (let i=stringlength; i > -1; i--) {
+        reverse += string.substr(i, 1);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
